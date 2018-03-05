805ddbafd7be58d631121ae55d27ed202a2ad03a
chenrui-hu <chenrui_hu1994@163.com>
2018-03-05 14:50:30 +0800

    user story 1&2

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,60 @@
 package com.twu.biblioteca;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
+    public static String getCustomer_name() {
+
+        Scanner sc = new Scanner(System.in);
+        String customer_name = sc.nextLine();
+        return customer_name;
+    }
+
+    public static String printWelcome(String customer_name) {
+
+        String welcome = "Hello " + customer_name + ". Welcome to Chenrui's Biblioteca, we are ready to serve you now.";
+        System.out.println(welcome);
+        return welcome;
+    }
+
+    public HashMap listBooks(HashMap bookList) {
+
+        for(Object keySet: bookList.keySet()){
+            System.out.print(keySet+"   ");
+            System.out.println(bookList.get(keySet));
+        }
+        return bookList;
+
+    }
+
+
+    public HashMap bookList(HashMap<Integer, ArrayList<String>> bookList, ArrayList<String> new_book){
+
+        Iterator iter = bookList.keySet().iterator();
+        int key = 1;
+        while(iter.hasNext()){
+            Object obj = iter.next();
+            key = Integer.parseInt(obj.toString());
+        }
+        bookList.put(key+1, new_book);
+
+        return bookList;
+    }
+
+//    public createBook(){
+//        ArrayList<String> book = new ArrayList<String>();
+//        book.add()
+//        return book;
+//    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        printWelcome(getCustomer_name());
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -3,11 +3,56 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void test_printWelcome() {
+
+        BibliotecaApp bib = new BibliotecaApp();
+        String welcome = "Hello Bill. Welcome to Chenrui's Biblioteca, we are ready to serve you now.";
+        assertEquals(welcome, bib.printWelcome("Bill"));
     }
+
+    @Test
+    public void test_getBookList(){
+        BibliotecaApp bib = new BibliotecaApp();
+        HashMap<Integer, ArrayList<String>> testBooks = new HashMap<Integer, ArrayList<String>>();
+        assertEquals(testBooks, bib.listBooks(testBooks));
+    }
+
+
+    @Test
+    public void test_bookList(){
+        BibliotecaApp bib = new BibliotecaApp();
+        HashMap<Integer, ArrayList<String>> testBooks = new HashMap<Integer, ArrayList<String>>();
+
+        ArrayList<String> first_bookInfos = new ArrayList<String>();
+        testBooks.put(1, first_bookInfos);
+
+        ArrayList<String> second_bookInfos = new ArrayList<String>();
+        testBooks.put(2, second_bookInfos);
+
+        ArrayList<String> third_bookInfos = new ArrayList<String>();
+        testBooks.put(3, third_bookInfos);
+
+        first_bookInfos.add("Head First Java");
+        first_bookInfos.add("Kathy Sierra; Bert Bates");
+        first_bookInfos.add("O'Reilly Media");
+
+        second_bookInfos.add("Test Driven Development");
+        second_bookInfos.add("Viktor Farcic; Alex Garcia");
+        second_bookInfos.add("Packt Publishing");
+
+        third_bookInfos.add("Think Python");
+        third_bookInfos.add("Allen B. Downey");
+        third_bookInfos.add("Green Tea Press");
+
+        assertEquals(testBooks, bib.listBooks(testBooks));
+    }
+
 }
